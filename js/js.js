0baddd48310075361e59080f5d25a27366ec6feb
Luis Fernando Uribe <luisferub@gmail.com>
2020-02-16 20:34:50 -0500

    Creacion js

---
+++ js/js.js
@@ -0,0 +1,3 @@
+var genero=["Accion","Ciencia Ficción","Terror","Comedia","Drama"];    
+
+var personajes=["John Wick","Neo", "John McClane", "Sarah Connor", "La novia", "Obi-Wan Kenobi", "Jake Sully", "Joseph Cooper", "Elliot", "Marty McFly", "Cindy Campbell", "Steve Stifler", "Stanley Ipkiss", "Alan Garner", "Peter Venkman", "Ed Warren", "Chris MacNeil", "Thomasin", "Jigsaw", "Pennywise", "Tony Montana", "Jack Dawson", "Wlagyslaw Szpilman", "Forrest Gump0", "Michael Corleone"];
