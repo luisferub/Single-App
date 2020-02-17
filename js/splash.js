3081acdc61345d44ec5ee31fa252e26aadcd3068
Luis Fernando Uribe <luisferub@gmail.com>
2020-02-16 20:25:07 -0500

    Primera Version

---
+++ js/splash.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    var contenedor = document.getElementById('splash');
+    contenedor.style.visibility='hidden';
+    //contenedor.style.opacity='0';
+
+	var genero=["Accion","Ciencia Ficción","Terror","Comedia","Drama"];    
+
+	var personajes=["John Wick","Neo", "John McClane", "Sarah Connor", "La novia", "Obi-Wan Kenobi", "Jake Sully", "Joseph Cooper", "Elliot", "Marty McFly", "Cindy Campbell", "Steve Stifler", "Stanley Ipkiss", "Alan Garner", "Peter Venkman", "Ed Warren", "Chris MacNeil", "Thomasin", "Jigsaw", "Pennywise", "Tony Montana", "Jack Dawson", "Wlagyslaw Szpilman", "Forrest Gump0", "Michael Corleone"];
+	
+}
