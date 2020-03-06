5aff8cf93e6016b89d38b34cfd100dc1b9870a09
ESTUDIANTES <luisferub@gmail.com>
2020-03-06 08:23:20 -0500

    Cambio - Imagenes de personajes

--- js/js.js
+++ js/js.js
@@ -1,15 +1,7 @@
 var secciones = [];
 		var tiempo_splash = 2000;
 
-		// Puntajes
-
-		var puntaje = 0;
-		var puntaje_accion = 0;
-		var puntaje_ficcion = 0;
-		var puntaje_comedia = 0;
-		var puntaje_terror = 0;
-		var puntaje_drama = 0;
-		
+		var puntaje = 0; // Puntaje
 		var contador = 0; // Contador de preguntas
 		var max = 10; // Maxima cantidad de preguntas
 
@@ -44,7 +36,8 @@ var secciones = [];
             if(id_seccion==3) {
                 document.getElementById("genero").innerHTML = personajes[lista[0]][1];
                 letra=personajes[lista[0]][2];
-                document.getElementById(letra).innerHTML = personajes[lista[0]][0];
+				document.getElementById(letra).innerHTML = personajes[lista[0]][0];
+				document.getElementById("personaje").src= personajes[lista[0]][5];
 			switch (letra) {
 				case "a":
 					document.getElementById("b").innerHTML = personajes[lista[0]][3];
@@ -72,11 +65,6 @@ var secciones = [];
 			}
 		
 			puntaje = 0;
-			puntaje_accion = 0;
-	 		puntaje_ficcion = 0;
-			puntaje_comedia = 0;
-			puntaje_terror = 0;
-			puntaje_drama = 0
 			contador = 0;
 
 		}
@@ -84,7 +72,7 @@ var secciones = [];
 		function clic(boton) {
 			if (aux==0){
 				elemento = document.getElementById(boton.id);
-         	    respuesta = document.getElementById(letra);
+				respuesta = document.getElementById(letra);
 				contador++;
 				aux = 1;
 				if((elemento.innerText)==(respuesta.innerText)){
@@ -102,24 +90,6 @@ var secciones = [];
 			puntaje++;
 			aux = 1;
 			genero = document.getElementById("genero");
-							
-			switch (genero.innerText) {
-				case "Accion":
-					puntaje_accion++;
-					break;
-				case "Ciencia ficcion":
-					puntaje_ficcion++;
-					break;
-				case "Comedia":
-					puntaje_comedia++;
-					break;
-				case "Terror":
-					puntaje_terror++;
-					break;
-				case "Drama":
-					puntaje_drama++;
-					break;
-			}
 			if (contador<max) {
 			setTimeout(continuarSiguiente, tiempo);
 				}
@@ -144,12 +114,29 @@ var secciones = [];
 			}
 			secciones[6].classList.remove("oculto");
 			elemento.style.background = 'white';//volver a poner el boton del color blanco
-			document.getElementById("puntaje").innerHTML = ('Total: ' + puntaje);
-			document.getElementById("result_1").innerHTML = ('Accion: ' + puntaje_accion);
-			document.getElementById("result_2").innerHTML = ('Ciencia Ficcion: ' + puntaje_ficcion);
-			document.getElementById("result_3").innerHTML = ('Comedia: ' + puntaje_comedia);
-			document.getElementById("result_4").innerHTML = ('Terror: ' + puntaje_terror);
-			document.getElementById("result_5").innerHTML = ('Drama: ' + puntaje_drama);
+			document.getElementById("puntaje").innerHTML = (puntaje + '/25');
+
+			if(puntaje<=5){
+				document.getElementById("men").innerHTML = mensaje[4][0];
+				document.getElementById("desc").innerHTML = mensaje[4][1];
+			}
+			if(puntaje>5&&puntaje<=10){
+				document.getElementById("men").innerHTML = mensaje[3][0];
+				document.getElementById("desc").innerHTML = mensaje[3][1];
+			}
+			if(puntaje>10&&puntaje<=15){
+				document.getElementById("men").innerHTML = mensaje[2][0];
+				document.getElementById("desc").innerHTML = mensaje[2][1];
+			}
+			if(puntaje>15&&puntaje<=20){
+				document.getElementById("men").innerHTML = mensaje[1][0];
+				document.getElementById("desc").innerHTML = mensaje[1][1];
+			}
+			if(puntaje>20&&puntaje<=25){
+				document.getElementById("men").innerHTML = mensaje[0][0];
+				document.getElementById("desc").innerHTML = mensaje[0][1];
+			}
+
         }
         var i = 1;
 		function continuarSiguiente() {
@@ -158,7 +145,8 @@ var secciones = [];
             	if(contador<max){
                 	document.getElementById("genero").innerHTML = personajes[lista[i]][1];
                 	letra=personajes[lista[i]][2];
-                	document.getElementById(letra).innerHTML = personajes[lista[i]][0];
+					document.getElementById(letra).innerHTML = personajes[lista[i]][0];
+					document.getElementById("personaje").src= personajes[lista[i]][5];
 			switch (letra) {
 				case "a":
 					document.getElementById("b").innerHTML = personajes[lista[i]][3];
@@ -180,180 +168,228 @@ var secciones = [];
 var lista = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 lista = lista.sort(function() {return Math.random() - 0.5});
 
+let mensaje = [
+	[
+		"¡Felicidades!",
+		"¡Eres un experto en cine!"
+	],
+	[
+		"¡Felicidades!",
+		"¡Te falta poco para convertirte en un maestro!"
+	],
+	[
+		"¡Vas bien!",
+		"¡Estás a mitad de camino de ser un experto en cine!"
+	],
+	[
+		"Ups...",
+		"Aún te faltan muchas películas por ver."
+	],
+	[
+		"Ups...",
+		"Quizás el cine no es lo tuyo..."
+	]
+];
+
 let personajes = [
     [
+        "El Resplandor",
+        "Terror",
+		"a",
+		"El bosque maldito",
+		"Mamá",
+		'img/the-shining.png'
+    ],
+    [
+        "Scary Movie",
+        "Comedia",
+		"c",
+		"¿Qué pasó ayer?",
+		"¿Y dónde está el piloto?",
+		'img/scary-movie.png'
+    ],
+    [
+        "The Matrix",
+        "Acción",
+		"b",
+		"El transportador",
+		"12 Balas",
+		'img/matrix.png'
+    ],
+    [
+        "It",
+        "Terror",
+		"c",
+		"Annabelle",
+		"The Witch",
+		'img/it.png'
+    ],
+    [
+        "Star Wars",
+        "Ciencia Ficción",
+		"b",
+		"Star Trek",
+		"Blade Runner",
+		'img/star-wars.png'
+    ],
+    [
+        "Scarface",
+        "Drama",
+		"a",
+		"El padrino",
+		"Pulp Fiction",
+		'img/scarface.png'
+    ],
+    [
+        "Titanic",
+        "Drama",
+		"c",
+		"El cisne negro",
+		"Siete almas",
+		'img/titanic.png'
+    ],
+    [
+        "Avatar",
+        "Ciencia Ficción",
+		"b",
+		"La llegada",
+		"Ex-Machina",
+		'img/avatar.png'
+    ],
+    [
         "John Wick",
-        "Accion",
+        "Acción",
+		"b",
+		"Alita",
+		"Ghost in shell",
+		'img/john-wick.png'
+    ],
+    [
+        "Viernes 13",
+        "Terror",
 		"a",
-		"Indiana jones",
-		"Bruce Banner"
+		"Halloween",
+		"San Valentín sangriento",
+		'img/viernes-13.png'
     ],
     [
-        "Neo",
-        "Accion",
+        "Saw",
+        "Terror",
 		"c",
-		"Han Solo",
-		"Frank Martin"
-    ],
-    [
-        "John McClane",
-        "Accion",
-		"b",
-		"Jhon Rambo",
-		"Jack Sparrow"
-    ],
-    [
-        "Sarah Connor",
-        "Accion",
-		"c",
-		"Alice Abernathy",
-		"Leia Organa"
-    ],
-    [
-        "La novia",
-        "Accion",
-		"b",
-		"Lara Croft",
-		"Agente Smith"
-    ],
-    [
-        "Obi-Wan Kenobi",
-        "Ciencia ficcion",
-		"a",
-		"Yoda",
-		"Darth Maul"
-    ],
-    [
-        "Jake Sully",
-        "Ciencia ficcion",
-		"c",
-		"Superman",
-		"Luke Skywalker"
-    ],
-    [
-        "Joseph Cooper",
-        "Ciencia ficcion",
-		"b",
-		"Depredador",
-		"Bib Fortuna"
-    ],
-    [
-        "Elliot",
-        "Ciencia ficcion",
-		"b",
-		"Star-Lord",
-		"Ellie Sattler"
-    ],
-    [
-        "Marty McFly",
-        "Ciencia ficcion",
-		"a",
-		"Rick Deckard",
-		"Fox Mulder"
-    ],
-    [
-        "Cindy Campbell",
-        "Comedia",
-		"c",
-		"Dana Scully",
-		"Dale Arden"
+		"Exterminio",
+		"Rec",
+		'img/saw.png'
     ],    
     [
-        "Steve Stifler",
-        "Comedia",
+        "E.T.",
+        "Ciencia Ficción",
 		"a",
-		"Less Grossman",
-		"Kenny McCormick"
+		"La Llegada",
+		"La Rebelión",
+		'img/et.png'
     ],
     [
-        "Stanley Ipkiss",
+        "La Máscara",
         "Comedia",
 		"c",
-		"Ace Ventura",
-		"Conde Olaf"
+		"Zoolander",
+		"Luna de miel en familia",
+		'img/la-mascara.png'
     ],    
-    [
-        "Alan Garner",
-        "Comedia",
-		"b",
-		"Fred Jones",
-		"Klaus Baudelaire"
-    ],
-    [
-        "Peter Venkman",
-        "Comedia",
-		"a",
-		"Arthur Poe",
-		"Waylon Smithers"
-    ],
-    [
-        "Ed Warren",
-        "Terror",
-		"b",
-		"Freddy Krueger",
-		"Regan MacNeil"
-    ],
-    [
-        "Chris MacNeil",
-        "Terror",
-		"a",
-		"Jack Torrance",
-		"Michael Mayers"
-    ],
-    [
-        "Thomasin",
-        "Terror",
-		"c",
-		"Jason Voorhees",
-		"Leatherface"
-    ],
-    [
-        "Jigsaw",
-        "Terror",
-		"b",
-		"The Candyman",
-		"Damien Thorn"
-    ],
-    [
-        "Pennywise",
-        "Terror",
-		"a",
-		"Frankestein",
-		"Mary Shaw"
-    ],
-    [
-        "Tony Montana",
-        "Drama",
-		"a",
-		"Norman Bates",
-		"Alex DeLarge"
-    ],
-    [
-        "Jack Dawson",
-        "Drama",
-		"a",
-		"Rick Blaine",
-		"Samara Morgan"
-    ],
-    [
-        "Wlagyslaw Szpilman",
-        "Drama",
-		"b",
-		"Severus Snape",
-		"Travis Bickle"
-    ],
     [
         "Forrest Gump",
-        "Drama",
-		"c",
-		"Atticus Finch",
-		"John Marston"
+        "Comedia",
+		"b",
+		"Roma",
+		"Yo antes de tí",
+		'img/forrest-gump.png'
     ],
     [
-        "Michael Corleone",
+        "El pianista",
         "Drama",
+		"a",
+		"Whiplash",
+		"The Help",
+		'img/el-pianista.png'
+    ],
+    [
+        "El exorcista",
+        "Terror",
 		"b",
-		"Hannibal Lecter",
-		"Darth Vader"
+		"La monja",
+		"El rito",
+		'img/el-exorcista.png'
+    ],
+    [
+        "Los Cazafantasmas",
+        "Comedia",
+		"a",
+		"Evolution",
+		"The blues brothers",
+		'img/cazafantasmas.png'
+    ],
+    [
+        "Duro de matar",
+        "Acción",
+		"c",
+		"Misión imposible",
+		"Salt",
+		'img/duro-de-matar.png'
+    ],
+    [
+        "Kill Bill",
+        "Acción",
+		"b",
+		"Death Proof",
+		"Grind House",
+		'img/kill-bill.png'
+    ],
+    [
+        "Terminator",
+        "Acción",
+		"a",
+		"Yo Robot",
+		"Robocop",
+		'img/terminator.png'
+    ],
+    [
+        "El Padrino",
+        "Drama",
+		"a",
+		"Brasco",
+		"El Irlandés",
+		'img/el-padrino.png'
+    ],
+    [
+        "El Conjuro",
+        "Terror",
+		"a",
+		"La noche del demonio",
+		"La crucifixión",
+		'img/el-conjuro.png'
+    ],
+    [
+        "Interstellar",
+        "Ciencia Ficción",
+		"b",
+		"Misión rescate",
+		"Ad Astra",
+		'img/interstellar.png'
+    ],
+    [
+        "¿Dónde están las rubias?",
+        "Comedia",
+		"c",
+		"Little Men",
+		"Norbit",
+		'img/donde-estan-las-rubias.png'
+    ],
+    [
+        "Volver al Futuro",
+        "Ciencia Ficción",
+		"b",
+		"Inception",
+		"Gravity",
+		'img/volver-al-futuro.png'
     ]
 ];
